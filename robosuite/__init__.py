d014d1795e9010eb1e3d5601ba0698476a859feb
Takuma Yoneda <takuma-yoneda@users.noreply.github.com>
2022-08-31 13:45:22 -0500

    add FixedPickPlace env

--- robosuite/__init__.py
+++ robosuite/__init__.py
@@ -12,6 +12,7 @@ from robosuite.environments.manipulation.two_arm_peg_in_hole import TwoArmPegInH
 from robosuite.environments.manipulation.two_arm_handover import TwoArmHandover
 from robosuite.environments.manipulation.block_insertion import BlockInsertion
 from robosuite.environments.manipulation.simple_pick_place import SimplePickPlace
+from robosuite.environments.manipulation.fixed_pick_place import FixedPickPlace
 
 from robosuite.environments import ALL_ENVIRONMENTS
 from robosuite.controllers import ALL_CONTROLLERS, load_controller_config
